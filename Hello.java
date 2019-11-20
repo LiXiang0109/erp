2607ddfc4e1ef109effbf3383933b6f18137be3a
mldn <lixiang15025187785@163.com>
2019-11-20 23:40:53 +0800

    Commit Work Dir File

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello(){
 	public static void main(String[] args) {
 		//System.out.println("Hello GIT!");
 		System.out.println("Today is Good!");
+		System.out.println("Tomorrow will be Good!");
 	}
 }

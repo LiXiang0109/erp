c09bdfff6d73ab008c269bf3ec6f07002524a60a
mldn <lixiang15025187785@163.com>
2019-11-21 21:07:42 +0800

    In Master change Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 public class Hello(){
 	public static void main(String[] args) {
-		//System.out.println("Hello GIT!");
-		System.out.println("Today is Good!");
 		System.out.println("Tomorrow will be Good!");
 		System.out.println("Hello New World!");
+		Admin admin = new Admin();
+		Emp emp = new Emp();
 	}
 }
